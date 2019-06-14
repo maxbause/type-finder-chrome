12c66b317483f07d4c5427f231a03eea49d17599
Max <maxbause@gmx.de>
2019-06-15 00:01:30 +0200

    added back to main view button to navigation

--- src/components/navigation/NavigationComponent.tsx
+++ src/components/navigation/NavigationComponent.tsx
@@ -1,13 +1,24 @@
 import * as React from "react";
+import { Link, RouteComponentProps, withRouter } from "react-router-dom";
 import "./navigation.scss";
 
-export default class NavigationComponent extends React.Component {
+class NavigationComponent extends React.Component<RouteComponentProps> {
   public render() {
+    const navigationLinkClasses = ["navigation__close-link"];
+    if (this.props.history.location.pathname === "/") {
+      navigationLinkClasses.push("navigation__close-link--hide");
+    }
+
     return (
       <div className="navigation">
         <img className="navigation__logo" src="assets/images/ts-logo.jpg" alt="TypeScript Logo"/>
         <div className="navigation__text">TypeFinder</div>
+        <Link to="/" className={navigationLinkClasses.join(" ")}>
+          <img className="navigation__close" src="assets/icons/close-white.svg" alt="Close" />
+        </Link>
       </div>
     );
   }
 }
+
+export default withRouter(NavigationComponent);
