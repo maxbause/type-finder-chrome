49b17e06edd3b7b58d4e80f0d006c84923b59168
Max <maxbause@gmx.de>
2019-06-14 23:37:56 +0200

    added settings hoc

--- src/components/code-box/CodeBoxComponent.tsx
+++ src/components/code-box/CodeBoxComponent.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import withPackageResolver, { ISearchResult, PackageState } from "../../container/PackageResolverContainer";
+import withSettings, { ISettings } from "../../container/SettingsContainer";
+import { compose } from "recompose";
 import "./code-box.scss";
 
-class CodeBoxComponent extends React.Component<ISearchResult> {
+class CodeBoxComponent extends React.Component<ISearchResult & ISettings> {
   public render() {
     const codeClasses = ["code-box__code", "code-box__code--loading"];
     const infoClasses = ["code-box__info", "code-box__info--loading"];
+
+    console.log(this.props);
+
     if (this.props.packageState !== PackageState.LOADING) {
       codeClasses.pop();
       infoClasses.pop();
@@ -40,8 +45,8 @@ class CodeBoxComponent extends React.Component<ISearchResult> {
       case PackageState.FOUND: {
         return (
           <div>
-            <div>yarn add -D <span className="code-box__code--bold">{this.props.packageName}</span></div>
-            <input id="code-copy" type="input" value={`yarn add -D ${this.props.packageName}`} readOnly={true} style={{position: "fixed", top: "-200vh", left: "-200vw"}} />
+            <div>{this.cliPrefix()}<span className="code-box__code--bold">{this.props.packageName}</span></div>
+            <input id="code-copy" type="input" value={`${this.cliPrefix()}${this.props.packageName}`} readOnly={true} style={{position: "fixed", top: "-200vh", left: "-200vw"}} />
           </div>
         );
       }
@@ -54,6 +59,17 @@ class CodeBoxComponent extends React.Component<ISearchResult> {
     }
   }
 
+  private cliPrefix(): string {
+    switch (this.props.packageCLI) {
+      case "npm": {
+        return "npm install --save-dev "
+      }
+      default: {
+        return "yarn add -D "
+      }
+    }
+  }
+
   private copy() {
     const elementToCopy = document.getElementById("code-copy") as HTMLInputElement;
     elementToCopy.select();
@@ -61,4 +77,4 @@ class CodeBoxComponent extends React.Component<ISearchResult> {
   }
 }
 
-export default withPackageResolver(CodeBoxComponent);
+export default compose(withPackageResolver, withSettings)(CodeBoxComponent);
