b56eb3dbc50b665ff268d4924d1e7ef1b08f4d7d
Max <maxbause@gmx.de>
2019-06-15 22:38:52 +0200

    fixed wording

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Building Type Finder chrome extension..."
-echo "[1/12] Installing resources..."
+echo "[1/4] Installing resources..."
 yarn install
 
-echo "[2/12] Building dist files"
+echo "[2/4] Building dist files"
 yarn run webpack:prod
 
-echo "[2/12] Bundeling files"
+echo "[2/4] Bundeling files"
 mkdir build/dist
 cp -a dist/. build/dist/
 
@@ -18,10 +18,10 @@ cp manifest.json build/manifest.json
 cp index.html build/index.html
 
 
-echo "[3/12] Compressing build"
+echo "[3/4] Compressing build"
 zip -r build/type_finder_chrome_extension.zip build/
 
-echo "[4/12] Cleaning up"
+echo "[4/4] Cleaning up"
 rm -r build/dist/
 rm -r build/assets/
 rm build/manifest.json
